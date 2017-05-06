d1e2b843605cb1c299609facbccbb454af5748a5
Kirill Batalin <kir55rus@gmail.com>
2017-05-07 02:03:32 +0700

    Add platform factory

---
+++ src/main/java/pro/batalin/ddl4j/platforms/PlatformFactoryException.java
@@ -0,0 +1,14 @@
+package pro.batalin.ddl4j.platforms;
+
+/**
+ * Created by Kirill Batalin (kir55rus) on 07.05.17.
+ */
+public class PlatformFactoryException extends Exception {
+    public PlatformFactoryException() {
+        super();
+    }
+
+    public PlatformFactoryException(String s) {
+        super(s);
+    }
+}
