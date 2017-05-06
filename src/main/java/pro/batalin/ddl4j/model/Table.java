8e27e194a988c0baf007b6408107bae9e379a2ed
lobkovilya <lobkovilya@yandex.ru>
2017-05-07 01:31:06 +0700

    Create table and column

--- src/main/java/pro/batalin/ddl4j/model/Table.java
+++ src/main/java/pro/batalin/ddl4j/model/Table.java
@@ -1,14 +1,28 @@
 package pro.batalin.ddl4j.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Kirill Batalin (kir55rus) on 06.05.17.
  */
-public interface Table extends Cloneable, SQLable {
-    void addColumn(Column column);
+public class Table implements Cloneable {
+    private List<Column> columns = new ArrayList<Column>();
 
-    List<Column> getColumns();
+    public void addColumn(Column column) {
+        columns.add(column);
+    }
 
-    Table clone() throws CloneNotSupportedException;
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    @Override
+    public Table clone() throws CloneNotSupportedException {
+        Table cloneTable = (Table)super.clone();
+        for (Column c : columns) {
+            cloneTable.addColumn(c.clone());
+        }
+        return cloneTable;
+    }
 }
