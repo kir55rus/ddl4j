61734d805f250a6ae8a98f6987820a1d9b36be2a
Kirill Batalin <kir55rus@gmail.com>
2017-05-20 22:24:36 +0700

    Add foreign key

---
+++ src/main/java/pro/batalin/ddl4j/model/constraints/ForeignKey.java
@@ -0,0 +1,62 @@
+package pro.batalin.ddl4j.model.constraints;
+
+import pro.batalin.ddl4j.model.Column;
+import pro.batalin.ddl4j.model.Table;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Kirill Batalin (kir55rus).
+ */
+public class ForeignKey {
+    private Table firstTable;
+    private Column firstColumn;
+    private Table secondTable;
+    private Column secondColumn;
+    private String name;
+
+    public ForeignKey(String name) {
+        this.name = name;
+    }
+
+    public Table getFirstTable() {
+        return firstTable;
+    }
+
+    public void setFirstTable(Table firstTable) {
+        this.firstTable = firstTable;
+    }
+
+    public Column getFirstColumn() {
+        return firstColumn;
+    }
+
+    public void setFirstColumn(Column firstColumn) {
+        this.firstColumn = firstColumn;
+    }
+
+    public Table getSecondTable() {
+        return secondTable;
+    }
+
+    public void setSecondTable(Table secondTable) {
+        this.secondTable = secondTable;
+    }
+
+    public Column getSecondColumn() {
+        return secondColumn;
+    }
+
+    public void setSecondColumn(Column secondColumn) {
+        this.secondColumn = secondColumn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
