d154827355e47cda3274c9b26e75f9768432dc93
Kirill Batalin <kir55rus@gmail.com>
2017-05-21 04:35:45 +0700

    Add annotation to converters

--- src/main/java/pro/batalin/ddl4j/platforms/oracle/converters/table/SQLDropTableConverter.java
+++ src/main/java/pro/batalin/ddl4j/platforms/oracle/converters/table/SQLDropTableConverter.java
@@ -1,12 +1,14 @@
 package pro.batalin.ddl4j.platforms.oracle.converters.table;
 
 import pro.batalin.ddl4j.model.Table;
+import pro.batalin.ddl4j.platforms.oracle.converters.Converter;
 import pro.batalin.ddl4j.platforms.oracle.converters.SQLConverter;
 import pro.batalin.ddl4j.platforms.statement_generator.NamedParameter;
 
 /**
  * Created by Kirill Batalin (kir55rus).
  */
+@Converter(modelClass = Table.class, action = "DROP")
 public class SQLDropTableConverter implements SQLConverter {
     private final String TEMPLATE = "DROP TABLE :table";
     private String table;
