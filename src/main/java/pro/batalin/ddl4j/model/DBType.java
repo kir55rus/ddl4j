7309ad94ae1239a1bc94081b67e8f979166ac639
Kirill Batalin <kir55rus@gmail.com>
2017-05-21 03:07:39 +0700

    Add db type class

---
+++ src/main/java/pro/batalin/ddl4j/model/DBType.java
@@ -0,0 +1,43 @@
+package pro.batalin.ddl4j.model;
+
+/**
+ * Created by Kirill Batalin (kir55rus).
+ */
+public class DBType {
+    private String type;
+
+    public DBType() {
+    }
+
+    public DBType(String type) {
+        setType(type);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type != null ? type.toUpperCase() : null;
+    }
+
+    @Override
+    public int hashCode() {
+        return type.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof DBType)) {
+            return false;
+        }
+
+        DBType dbType = (DBType) o;
+
+        if (type == null) {
+            return dbType.type == null;
+        }
+
+        return type.equals(dbType.type);
+    }
+}
