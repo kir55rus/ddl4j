6cb70c602b1be216fd25da249dbd672dd33ccbd3
Kirill Batalin <kir55rus@gmail.com>
2017-05-20 18:21:06 +0700

    Refactor primary key

--- src/main/java/pro/batalin/ddl4j/model/PrimaryKey.java
+++ src/main/java/pro/batalin/ddl4j/model/PrimaryKey.java
@@ -7,21 +7,34 @@ import java.util.List;
 /**
  * Created by Kirill Batalin (kir55rus).
  */
-public class PrimaryKey {
+public class PrimaryKey extends Constraint {
     private Table table;
     private List<Column> columns;
 
     public PrimaryKey() {
+        this(null, (Column) null);
+    }
+
+    public PrimaryKey(String name) {
+        this(name, null, (Column) null);
     }
 
     public PrimaryKey(Table table, List<Column> columns) {
-        this.table = table;
-        this.columns = columns;
+        this(null, table, columns);
     }
 
     public PrimaryKey(Table table, Column column) {
+        this(null, table, column);
+    }
+
+    public PrimaryKey(String name, Table table, Column column) {
+        this(name, table, Collections.singletonList(column));
+    }
+
+    public PrimaryKey(String name, Table table, List<Column> columns) {
+        super(ConstraintType.PRIMARY_KEY, name);
         this.table = table;
-        this.columns = Collections.singletonList(column);
+        this.columns = columns;
     }
 
     public Table getTable() {
